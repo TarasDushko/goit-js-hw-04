60df46ae55a16655d109f378da764fdacf4577a3
Taras <tarasdushko@gmail.com>
2025-04-28 20:33:32 +0200

    fixed mistakes

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,27 @@
 function isEnoughCapacity(products, containerSize) {
-  const totalProducts = Object.values(products).reduce(
-    (sum, quantity) => sum + quantity,
-    0
-  );
+  let sum = 0;
+  const quantities = Object.values(products);
+  for (let i = 0; i < quantities.length; i++) {
+    sum += quantities[i];
+  }
 
-  return totalProducts <= containerSize;
+  return sum <= containerSize;
 }
 
+//function isEnoughCapacity(products, containerSize) {
+//let totalProducts = 0;
+
+// for (let product in products) {
+// totalProducts += products[product];
+//}
+
+//switch (true) {
+//case totalProducts <= containerSize:
+//return true;
+//default:
+//return false;
+//}
+//}
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
